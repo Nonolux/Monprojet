589988334c5ff6600d58957427450c2c5edbb2dc
Norbert <noben@tango.lu>
2018-04-19 23:30:40 +0200

    Premier script fonctionel, problem avec les points derière la virgule

--- maison.sh
+++ maison.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 message='Materiel pour votre maison'
 echo $message
+read -p 'Entrez la largeur:' H
+read -p 'Entrez la longeur:' L
+let "c = H * L "
+let "ca = c / 2"
+let "b = H * L"
+let "be = b / 10"
+echo "Vous avez besoin de $ca métres cube de cailloux"
+echo "Vous avez besoin de $be métres cube de beton"
+
+
 
 
 
