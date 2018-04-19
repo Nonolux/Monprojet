8cc6f6fdd091e999ac81c00f4fa778e52c526a90
Norbert <noben@tango.lu>
2018-04-20 00:41:29 +0200

    script fonctionel, problem avec la virgule en entrant les données

--- maison.sh
+++ maison.sh
@@ -3,12 +3,12 @@ message='Materiel pour votre maison'
 echo $message
 read -p 'Entrez la largeur:' H
 read -p 'Entrez la longeur:' L
-let "c = H * L "
-let "ca = c / 2"
-let "b = H * L"
-let "be = b / 10"
-echo "Vous avez besoin de $ca métres cube de cailloux"
-echo "Vous avez besoin de $be métres cube de beton"
+echo -n "Vous avez besoin de "
+echo "scale=3; $H*$L * 0.5" | bc  
+echo "métres cube de cailloux"
+echo -n "Vous avez besoin de "
+echo "scale=3; $H*$L * 0.1" | bc
+echo "métres cube de beton"
 
 
 
